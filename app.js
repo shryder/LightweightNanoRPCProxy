7bb25df7dfcb67591bf13399ae0da2523cf3cc76
Shryder <Shryder1337@gmail.com>
2021-05-03 03:38:56 +0000

    Initial code commit

---
+++ app.js
@@ -0,0 +1,141 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+let STATS = require('./logs.json');
+
+const {
+	AVAILABLE_ACTIONS,
+	REQUESTS_LIMIT,
+	USERS,
+	PORT,
+	API_ROUTE,
+	NODE_RPC,
+	LOG_DATA
+} = require('./config.json');
+
+const DEFAULT_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+	'Content-Type': 'application/json'
+};
+
+const { RateLimiterMemory } = require('rate-limiter-flexible');
+const rateLimiter = new RateLimiterMemory({
+	points: REQUESTS_LIMIT,
+	duration: 3600,
+});
+
+const NanoClient = require('nano-node-rpc');
+const client = new NanoClient({ url: NODE_RPC });
+
+app.use(express.json());
+app.set('trust proxy', 'loopback');
+
+function setRateLimitHeaders(res, rateLimiterRes) {
+	res.set({
+		"Retry-After": rateLimiterRes.msBeforeNext / 1000,
+		"X-RateLimit-Limit": REQUESTS_LIMIT,
+		"X-RateLimit-Remaining": rateLimiterRes.remainingPoints,
+		"X-RateLimit-Reset": new Date(Date.now() + rateLimiterRes.msBeforeNext)
+	});
+}
+
+/**
+ * Returns user object if token is valid, returns undefined if invalid token is provided
+ * @param {string} token 
+ */
+function getUserByToken(token) {
+	return USERS.find(user => user.token == token);
+}
+
+if (LOG_DATA) {
+	// Save logs every 10 minutes
+	setInterval(() => {
+		fs.writeFile('logs.json', JSON.stringify(STATS), (err) => {
+			if (err) {
+				console.error("Error writing stats to file.", err);
+			}
+		});
+	}, 10 * 60 * 1000);
+}
+
+async function handleRPCRequest(req, res) {
+    res.set(DEFAULT_HEADERS);
+
+	if (LOG_DATA) {
+		res.on('finish', function(e){
+			if(req.ip in STATS) {
+				if (res.statusCode in STATS[req.ip]) {
+					STATS[req.ip][res.statusCode] = STATS[req.ip][res.statusCode] + 1;
+				} else {
+					STATS[req.ip][res.statusCode] = 1;
+				}
+	
+				STATS[req.ip].lastRequest = Date.now();
+			} else {
+				STATS[req.ip] = {
+					[res.statusCode]: 1,
+					firstRequest: Date.now(),
+					lastRequest: Date.now()
+				};
+			}
+		});
+	}
+	
+	if(!("action" in req.body)) {
+		return res.status(422).json({
+			message: "Action field is required"
+		});
+	}
+
+	const action = req.body.action;
+	const authorization_header = req.header('Authorization');
+
+	let allowed_actions = [...AVAILABLE_ACTIONS];
+
+	if(authorization_header) {
+		let user = getUserByToken(authorization_header);
+
+		if (user) {
+			allowed_actions.concat(user.extra_available_actions);
+		} else {
+			return res.status(403).json({
+				message: "Invalid authorization token provided."
+			});
+		}
+	} else {
+		try {
+			const rateLimiterRes = await rateLimiter.consume(req.ip, 1);
+	
+			setRateLimitHeaders(res, rateLimiterRes);
+		} catch(rateLimiterRes) {
+			setRateLimitHeaders(res, rateLimiterRes);
+
+			return res.status(429).json({
+				message: "Too Many Requests"
+			});
+		}
+	}
+
+	if (!allowed_actions.includes(action)) {
+		return res.status(403).json({
+			message: "Action is not allowed"
+		});
+	}
+
+	let params = Object.assign({ }, req.body);
+	delete params.action;
+
+	const rpc_response = await client._send(action, params);
+	return res.json({
+		message: "wicked"
+	});
+}
+
+app.get(API_ROUTE, handleRPCRequest);
+app.post(API_ROUTE, handleRPCRequest);
+
+app.listen(PORT, () => {
+	console.log(`RPC handler app listening at port ${PORT}`)
+});
